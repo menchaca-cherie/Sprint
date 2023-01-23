9bcc8cc447afe4e67041bcb67a7a27b6a12a35a7
menchaca-cherie <91163556+menchaca-cherie@users.noreply.github.com>
2023-01-22 18:33:18 -0800

    Delete Sprint directory

--- Sprint/js/functions.js
+++
@@ -1,6 +0,0 @@
-function copyURL() {
-    var copyText = document.getElementById("url");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-}

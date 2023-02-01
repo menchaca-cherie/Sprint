833c57aa7f9420ac29f570b95ae75ee2edb702c6
menchaca-cherie <91163556+menchaca-cherie@users.noreply.github.com>
2023-01-31 22:20:20 -0800

    Add files via upload
    I am adding files manually.

--- advisor_home.php
+++ advisor_home.php
@@ -1,35 +1,99 @@
+<?php
+
+include("php/connect.php");
+include('./php/token.php');
+session_start();
+
+//Initialize variables
+$uun = "";
+$validLogin = true;
+
+//If the user is already loggeg in
+if(isset($_SESSION['username'])){
+    //Redirect to homepage
+    header(('location: advisor_home.php'));
+}
+
+if(!empty($_POST)){
+    //get the form data
+    $uun = $_POST['username'];
+    $pw = $_POST['password'];
+
+    require('php/admin_creds.php');
+
+    //If the username is the array and the passwords match
+    if(array($uun, $login) && $pw == $login[$uun]){
+        //Records username in the session array
+        $_SESSION['username']= $uun;
+
+        //Go to the page that the user came from, or else the home page
+        $page = isset($_SESSION['page']) ? $_SESSION['page'] : 'admin.php';
+        header('location: ' .$page);
+    }
+    //Invalid login -- set flag variable
+    $validLogin = false;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="styles/student_styles.css">
-  <title> Advise-It Homepage </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/student_styles.css">
+    <title> Advise-It Homepage </title>
 
 </head>
-<body>
-
-<h1 class="mb-auto text-center text-nowrap no-mobile"> Advise-It Homepage </h1>
-<div class="position-relative text-center my-5">
-  <div class="col mx-2">
-    <a href="student_schedule.php" class="btn btn-outline-primary">Create New Schedule</a>
-  </div>
-  <form action="" method="get">
+    <body>
     <div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+        <h1 class="mb-auto text-center text-nowrap no-mobile"> Advise-It Homepage </h1>
     </div>
-  </form>
-  <form action="" method="post" id="save">
-    <div class="row">
-      <div class="col-md-12">
-        <hr>
+    <?php
+    $uni_urlToken= generateToken();
+    //Append it to the end of URL
 
+    $uni_urlToken = "student_schedule.php/".$uni_urlToken;
 
-
-      </div>
+    ?>
+    <!--Create Button will take you to student_schedule page    -->
+    <div class="form-group text-center my-5">
+        <?php echo '<form action="'.$uni_urlToken.'"method="post">
+                    <input type="hidden" name="token" value="'.$uni_urlToken.'">
+                    <button class="btn btn-outline-primary px-5" type="submit" id="create">Create New</button>
+                </form>'
+        ?>
     </div>
-  </form>
-</div>
-</body>
+    <!--Admin Login    -->
+    <form action="" method="post" id="login" autocomplete="off" class="bg-light border p-3">
+        <div class="form-row justify-content-center">
+            <h4 class="title my-3">Login Administration</h4>
+            <div class="col-6">
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1"><i class="fas fa-user"></i></span>
+                    </div>
+                    <div class="form-group text-center my-5">
+                        <div class="row input-group mb-3">
+                            <input type="text" class="form-control" id="username" name="username" placeholder="username"
+                                    value="<?php echo $uun; ?>"
+                        </div>
+                    </div>
+                    <div class="form-group text-center my-5">
+                        <div class="row input-group mb-3">
+                            <input type="password" class="form-control" id="password" name="password" placeholder="password">
+                        </div>
+                    </div>
+                    <?php
+                    if(!$validLogin){
+                        echo'<p class="err">Login is incorrect</p>';
+                    }
+                    ?>
+                    <div>
+                        <button type="submit" class="btn btn-dark">Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
--- student_schedule.php
+++ student_schedule.php
@@ -1,39 +1,31 @@
 <?php
 //include token function
-include('./php/token.php');
+
 include('./php/connect.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/../config.php');
 
 $uri = $_SERVER['REQUEST_URI'];
-$tokenID = (substr($uri, -6));//last six characters token
+$token = (substr($uri, -6));//last six characters token
 
 //Initiate variables
-$tokenID = "";
+
 $fall = "";
 $winter = "";
 $spring = "";
 $summer = "";
+$advisor = "";
 $date = "";
 
-$query = "SELECT * FROM advise_it ORDER BY '$tokenID'";
+
+$query = "SELECT * FROM advise_it WHERE token ='$token'";
 $query_run = mysqli_query($cnxn, $query);
 
 if(mysqli_num_rows($query_run) == 0)
 {
-    $sql = "INSERT INTO advise_it ('tokenID') VALUES ('$tokenID')";
+    $sql = "INSERT INTO advise_it ('token') VALUES ('$token')";
     $connect = mysqli_query($cnxn, $sql);
     $query_statement = $dbh->prepare($sql);
     $query_statement->execute();
-}
-
-
-
-//testing
-if (!empty($tokenID)) {
-
-    validateToken($tokenID);
-}
-else {
-    $tokenID = generateToken();
 }
 
 ?>
@@ -45,105 +37,190 @@ else {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles/student_styles.css">
+    <link rel="stylesheet" href="../styles/student_styles.css">
+
     <title>Student Schedule</title>
 </head>
 
 <body>
-<button href="advisor_home.php" class="btn btn-primary btn-lg ml-5">Back</button>
+
 <!--H1 Title-->
 <h1 class="mb-auto text-center text-nowrap no-mobile">Student Schedule</h1>
-
+<!--Token copy URL with the token -->
 <div class="row justify-content-center">
-    <input type="text" id="url" value="https://menchaca.cherie.greenriverdev.com/485/Sprint/student_schedule.php/?tokenID=<?php echo $tokenID; ?>">
+    <input type="text" id="url" value="https://menchaca.cherie.greenriverdev.com/485/Sprint/student_schedule.php/token = <?php echo $token; ?>">
     <button onclick="copyURL()">Copy</button>
 </div>
 <div class="row justify-content-center my-2" >
     <h3 class="mx-2">Token: </h3>
-    <h3 id="tokenDisplay"><?php echo $tokenID; ?></h3>
+    <h3 id="tokenDisplay"><?php echo $token; ?></h3>
 </div>
+<!--Token ID-->
+    <input type="hidden" name="token" value="<?php echo $token ?>">
 <!--Form id, action, method-->
-<form id="student_schedule" action="confirmation.php" method="post">
-
-    <input type="hidden" name="tokenID" value="<?php echo $tokenID ?>">
-
     <form action="" method="post" id="save">
 
+            <?php
+
+            $query = "SELECT * FROM advise_it WHERE token = '$token'";
+            $result = mysqli_query($cnxn, $query);
+
+            if(mysqli_num_rows($result)== 0) {
+                foreach ($result as $row) {
+                    $fall = $row['fall'];
+                    $winter = $row['winter'];
+                    $spring = $row['spring'];
+                    $summer = $row['summer'];
+                    $advisor = $row['advisor'];
+
+                }
+            }
+            ?>
+            <div class="row">
+                <div class="col text-center my-1">
+                    <h5 class="text-center">Advisor: </h5>
+                    <textarea id="advisor"  name="advisor" rows="1"><?php
+                        echo $advisor;?>Name:</textarea>
+                </div>
+            </div>
+    <!--Starting cards first row-->
+            <div class="row">
+                <div class="col-sm-5 my-3 mx-auto">
+                    <div class="card text-white bg-secondary">
+                        <h5 class="card-header text-center">Fall: </h5>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6 text-center">
+                                    <textarea id="fall"  name="fall" rows="4">Classes:<?php echo $fall; ?></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-5 my-3 mx-auto">
+                    <div class="card text-white bg-secondary">
+                        <h5 class="card-header text-center">Winter: </h5>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6 text-center">
+                                    <textarea id="winter"  name="winter" rows="4">Classes:<?php echo $winter; ?></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Starting card 2nd row-->
+            <div class="row">
+                <div class="col-sm-5 my-3 mx-auto">
+                    <div class="card text-white bg-secondary">
+                        <h5 class="card-header text-center">Spring: </h5>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6 text-center">
+                                    <textarea id="spring"  name="spring" rows="4">Classes:<?php echo $spring; ?></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-5 my-3 mx-auto">
+                    <div class="card text-white bg-secondary">
+                        <h5 class="card-header text-center">Summer: </h5>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6 text-center">
+                                    <textarea id="summer"  name="summer" rows="4">Classes:<?php echo $summer; ?></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row my-5">
+                <div class="col-sm-12 my-5">
+                    <div>
+                        <button type="submit" class="btn btn-primary btn-lg mr-5 float-right px-5">Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         <?php
 
-        $query = "SELECT * FROM student_plan WHERE token = '$tokenID'";
-        $result = mysqli_query($cnxn, $query);
 
-        if(mysqli_num_rows($result              ) > 0)
-        {
-            foreach ($result as $row) {
-                $fall = $row['fall'];
-                $winter = $row['winter'];
-                $spring = $row['spring'];
-                $summer = $row['summer'];
-            }
+        //Insert Schedule
+        //1. Define the query
+//        $sql_add = "INSERT INTO advise_it (`token`, `fall`, `winter`, `spring`, `summer`)
+//        VALUES ('$token' ,'$fall', '$winter' , '$spring', '$summer')";
+
+        $sql_add = "INSERT INTO advise_it (token, fall, winter, spring, summer,advisor) 
+       VALUES (:token ,:fall, :winter , :spring, :summer, :advisor)";
+
+//        if ($cnxn->query($sql_add) === TRUE) {
+//            $fall = $_POST['fall'];
+//            $winter = $_POST['winter'];
+//            $spring = $_POST['spring'];
+//            $summer = $_POST['summer'];
+//            $advisor = $_POST['advisor'];
+//        }
+//
+
+        //2. Prepare the statement
+        $statement = mysqli_query($cnxn, $sql_add);
+        $query_statement = $dbh->prepare($sql_add);
+
+        if(isset($fall)){
+            $fall = $_POST[$fall];
         }
-        ?>
+        if(isset($winter)){
+            $winter = $_POST[$winter];
+        }
+        if(isset($spring)){
+            $spring = $_POST[$spring];
+        }
+        if(isset($summer)){
+            $summer = $_POST[$summer];
+        }
+        if (isset($advisor)) {
+            $advisor = $_POST[$advisor];
+        }
 
-        <!--Starting cards first row-->
-        <div class="row">
-            <div class="col-sm-5 my-5 mx-auto">
-                <div class="card text-white bg-secondary">
-                    <h5 class="card-header text-center">Fall</h5>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6 text-center">
-                                <textarea id="fall" name="fall" rows="4"><?php echo $fall; ?></textarea>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-5 my-5 mx-auto">
-                <div class="card text-white bg-secondary">
-                    <h5 class="card-header text-center">Winter</h5>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6 text-center">
-                                <textarea id="winter"  name="winter" rows="4"><?php echo $winter; ?></textarea>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--Starting card 2nd row-->
-        <div id="card2" class="row">
-            <div class="col-sm-5 my-3 mx-auto">
-                <div class="card text-white bg-secondary">
-                    <h5 class="card-header text-center">Spring</h5>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6 text-center">
-                                <textarea id="spring" name="spring" rows="4"><?php echo $spring; ?></textarea>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-5 my-3 mx-auto">
-                <div class="card text-white bg-secondary">
-                    <h5 class="card-header text-center">Summer</h5>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6 text-center">
-                                <textarea id="summer"  name="summer" rows="4"><?php echo $summer; ?></textarea>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--Primary Save Button for all quarters-->
-        <div class="bottom-0 end-0 my-2 mx-auto">
-            <button type="submit" class="btn btn-primary btn-lg mr-5 float-right">Save</button>
-        </div>
-    </form>
+        $query_statement->bindParam('token', $token, PDO::PARAM_STR);
+        $query_statement->bindParam('fall', $fall, PDO::PARAM_STR);
+        $query_statement->bindParam('winter', $winter, PDO::PARAM_STR);
+        $query_statement->bindParam('spring', $spring, PDO::PARAM_STR);
+        $query_statement->bindParam('summer', $summer, PDO::PARAM_STR);
+        $query_statement->bindParam('advisor', $advisor, PDO::PARAM_STR);
+
+
+        //Time and update time
+
+//        $date = time();
+//        $date_update = date("Y-m-d h:m:s", $date);
+
+        //statement to use the date/time
+
+//        $update_current = "UPDATE advise_it
+//        SET fall ='$fall', winter ='$winter', spring = '$spring', summer = '$summer',time = '$date_update'
+//        WHERE token = '$token'";
+
+//        $update_statement = $dbh->prepare($update_current);
+//        $update_statement ->execute();
+//
+//        if(mysqli_num_rows($result) > 0)
+//        {
+//            foreach ($result as $row)
+//            {
+//                $save_current_time = $row['time'];
+//
+//                echo '<p id="time" > Update: '.$save_current_time.'</p>';
+//
+//            }
+//
+//        }
+        ?>
+    <!--Primary Save Button for all quarters-->
+
 </form>
 
 <script src="js/functions.js"></script>

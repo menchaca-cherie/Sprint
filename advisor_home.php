bf35e46d3e7e53825f67379687d55ce3f2d277d0
menchaca-cherie <91163556+menchaca-cherie@users.noreply.github.com>
2023-01-31 22:18:41 -0800

    Add files via upload
    Adding manual files to my git hub due to not being able to connect to github

--- advisor_home.php
+++ advisor_home.php
@@ -1,43 +1,35 @@
-<?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-include('./php/token.php');
-
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles/student_styles.css">
-    <title> Advise-It Homepage </title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles/student_styles.css">
+  <title> Advise-It Homepage </title>
 
 </head>
-    <body>
+<body>
+
+<h1 class="mb-auto text-center text-nowrap no-mobile"> Advise-It Homepage </h1>
+<div class="position-relative text-center my-5">
+  <div class="col mx-2">
+    <a href="student_schedule.php" class="btn btn-outline-primary">Create New Schedule</a>
+  </div>
+  <form action="" method="get">
     <div>
-        <h1 class="mb-auto text-center text-nowrap no-mobile"> Advise-It Homepage </h1>
+      <button type="submit" class="btn btn-primary">Submit</button>
     </div>
-    <?php
+  </form>
+  <form action="" method="post" id="save">
+    <div class="row">
+      <div class="col-md-12">
+        <hr>
 
-    if (!empty($tokenID)) {
 
-        validateToken($tokenID);
-    }
-    else {
-        $tokenID = generateToken();
 
-    }
-    $uni_url = "student_schedule.php/".$tokenID;
-
-    ?>
-    <div class="position-relative text-center my-5">
-        <?php echo '<form action='.$uni_url.'method="post">
-                    <button class="btn btn-outline-primary px-5" type="submit" id="create">Create New</button>
-                </form>'
-        ?>
+      </div>
     </div>
-
-    </body>
+  </form>
+</div>
+</body>
 </html>
